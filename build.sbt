2568b1e463275b84b6b5ffb8b780037f6d217a35
Miguel C <mkarmona@gmail.com>
2021-04-26 16:41:24 +0100

    feat: implement relevance field per publication-keyword

--- build.sbt
+++ build.sbt
@@ -16,7 +16,7 @@ lazy val root = (project in file("."))
       )
     ),
     name := "io-opentargets-etl-literature",
-    version := "1.9",
+    version := "1.10",
     resolvers ++= buildResolvers,
     libraryDependencies ++= loggingDeps,
     libraryDependencies ++= sparkDeps,
--- src/main/scala/io/opentargets/etl/literature/Processing.scala
+++ src/main/scala/io/opentargets/etl/literature/Processing.scala
@@ -6,8 +6,17 @@ import org.apache.spark.sql.functions._
 import io.opentargets.etl.literature.spark.Helpers
 import io.opentargets.etl.literature.spark.Helpers.IOResource
 import org.apache.spark.sql._
+import org.apache.spark.sql.expressions.Window
+import org.apache.spark.sql.types.{DoubleType, LongType}
 
 object Processing extends Serializable with LazyLogging {
+
+  private def harmonicFn(c: Column): Column =
+    aggregate(
+      zip_with(c, sequence(lit(1), size(c)), (e1, e2) => e1 / pow(e2, 2D)),
+      lit(0D),
+      (c1, c2) => c1 + c2
+    )
 
   private def filterCooccurrences(df: DataFrame, isMapped: Boolean)(
       implicit sparkSession: SparkSession): DataFrame = {
@@ -34,6 +43,91 @@ object Processing extends Serializable with LazyLogging {
     df.selectExpr("*", "match.*")
       .drop(droppedCols: _*)
       .filter($"isMapped" === isMapped)
+  }
+
+  private def filterMatchesForCH(df: DataFrame)(implicit context: ETLSessionContext): DataFrame = {
+    import context.sparkSession.implicits._
+
+    val sectionImportances = context.configuration.common.publicationSectionRanks
+
+    val sectionRankTable =
+      broadcast(
+        sectionImportances
+          .toDS()
+          .orderBy($"rank".asc))
+
+    val wBySectionKeyword = Window.partitionBy("pmid", "section", "keywordId")
+    val wByKeyword = Window.partitionBy("pmid", "keywordId")
+    val wBySection = Window.partitionBy("pmid", "section")
+    val wByPmid = Window.partitionBy("pmid")
+
+    val cols = List(
+      "pmid",
+      "pmcid",
+      "date",
+      "year",
+      "month",
+      "day",
+      "keywordIds",
+      "keywordId",
+      "relevance",
+      "keywordType",
+      "maxSectionFreq",
+      "keywordSectionFreq",
+      "label",
+      "sentences"
+    )
+
+    val fdf = df
+      .withColumn("pmid", $"pmid".cast(LongType))
+      .withColumnRenamed("type", "keywordType")
+
+    val sentencesDF = fdf
+      .filter($"section".isInCollection(Seq("title", "abstract")))
+      .groupBy($"pmid", $"section")
+      .agg(
+        struct(
+          $"section",
+          collect_list(
+            struct($"label",
+                   $"keywordType",
+                   $"keywordId",
+                   $"startInSentence",
+                   $"endInSentence",
+                   $"sectionStart",
+                   $"sectionEnd")).as("matches")
+        ).as("sentencesBySection")
+      )
+      .groupBy($"pmid")
+      .agg(to_json(collect_list($"sentencesBySection")).as("sentences"))
+
+    fdf
+      .join(sectionRankTable, Seq("section"), "left_outer")
+      .na
+      .fill(100, "rank" :: Nil)
+      .na
+      .fill(0.01, "weight" :: Nil)
+      .withColumn("keywordSectionFreq", count(lit(1)).over(wBySectionKeyword))
+      .withColumn("maxSectionFreq", max($"keywordSectionFreq").over(wBySection))
+      .withColumn(
+        "scoreByKeywordSection",
+        when(
+          $"maxSectionFreq" > 0,
+          $"keywordSectionFreq".cast(DoubleType) / $"maxSectionFreq".cast(DoubleType) * $"weight")
+          .otherwise(0D))
+      .dropDuplicates("pmid", "section", "keywordId")
+      .withColumn(
+        "relevance",
+        round(harmonicFn(
+                collect_list($"scoreByKeywordSection")
+                  .over(wByKeyword.orderBy($"rank"))
+              ),
+              4)
+      )
+      .dropDuplicates("pmid", "keywordId")
+      .withColumn("keywordIds", collect_set($"keywordId").over(wByPmid))
+      .join(sentencesDF, Seq("pmid"), "left_outer")
+      .selectExpr(cols: _*)
   }
 
   private def aggregateMatches(df: DataFrame)(implicit sparkSession: SparkSession): DataFrame = {
@@ -105,7 +199,7 @@ object Processing extends Serializable with LazyLogging {
     logger.info("Processing coOccurences calculate done")
     val coocs = filterCooccurrences(grounding("cooccurrences"), isMapped = true)
 
-    val literatureIndex = matches.transform(aggregateMatches)
+    val literatureIndexAlt = matches.transform(filterMatchesForCH)
 
     val outputs = empcConfiguration.outputs
     logger.info(s"write to ${context.configuration.common.output}/matches")
@@ -118,7 +212,7 @@ object Processing extends Serializable with LazyLogging {
         outputs.matches.copy(path = context.configuration.common.output + "/failedCooccurrences")),
       "cooccurrences" -> IOResource(coocs, outputs.cooccurrences),
       "matches" -> IOResource(matches, outputs.matches),
-      "literatureIndex" -> IOResource(literatureIndex, outputs.literatureIndex)
+      "literatureIndex" -> IOResource(literatureIndexAlt, outputs.literatureIndex)
     )
 
     Helpers.writeTo(dataframesToSave)
