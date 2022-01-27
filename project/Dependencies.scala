208327b5e3e9d942bebb652e6ecd7875e37b70ad
Jarrod Baker <jarrod.bakeriii@gmail.com>
2022-01-27 11:35:51 +0000

    Update dependencies to reduce jar size.
    We don't need to bundle test or Spark dependencies for dataproc.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,12 +2,6 @@ import sbt._
 
 object Dependencies {
 
-  lazy val aoyi = Seq(
-    "com.lihaoyi" %% "pprint" % "0.6.0"
-  )
-
-  lazy val betterFiles = "com.github.pathikrit" %% "better-files-akka" % "3.9.1"
-
   lazy val configDeps = Seq(
     "com.github.pureconfig" %% "pureconfig" % "0.14.1"
   )
@@ -20,17 +14,17 @@ object Dependencies {
   lazy val sparkVersion = "3.1.2"
   lazy val sparkDeps = Seq(
     "com.github.fommil.netlib" % "all" % "1.1.2" pomOnly (),
-    "org.apache.spark" %% "spark-core" % sparkVersion,
-    "org.apache.spark" %% "spark-sql" % sparkVersion,
-    "org.apache.spark" %% "spark-graphx" % sparkVersion,
-    "org.apache.spark" %% "spark-mllib" % sparkVersion
+    "org.apache.spark" %% "spark-core" % sparkVersion % "provided",
+    "org.apache.spark" %% "spark-sql" % sparkVersion % "provided",
+    "org.apache.spark" %% "spark-graphx" % sparkVersion % "provided",
+    "org.apache.spark" %% "spark-mllib" % sparkVersion % "provided"
   )
 
-  lazy val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.14.3"
+  lazy val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.14.3" % "test"
 
   lazy val testVersion = "3.2.0"
   lazy val testingDeps = Seq(
-    "org.scalactic" %% "scalactic" % testVersion,
+    "org.scalactic" %% "scalactic" % testVersion % "test",
     "org.scalatest" %% "scalatest" % testVersion % "test"
   )
 
